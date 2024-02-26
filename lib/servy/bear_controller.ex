cd6d2fdb71746e7482f59d85b4543da9d7637272
Fabricio Damazio <fabridamazio@gmail.com>
2024-02-26 12:46:10 -0300

    Class 16 - Comprehensions

--- lib/servy/bear_controller.ex
+++ lib/servy/bear_controller.ex
@@ -1,21 +1,22 @@
 defmodule Servy.BearController do
+  alias Servy.BearView
   alias Servy.Bear
   alias Servy.Conv
   alias Servy.Wildthings
+  require BearView
 
   def index(%Conv{} = conv) do
-    items =
+    bears =
       Wildthings.list_bears()
       |> Enum.sort(&Bear.order_asc_by_name/2)
-      |> Enum.map(&bear_item/1)
-      |> Enum.join()
 
-    %Conv{conv | status: 200, resp_body: "<ul>#{items}</ul>"}
+      %Conv{conv | status: 200, resp_body: BearView.index(bears) }
   end
 
   def show(%Conv{} = conv, %{"id" => id}) do
     bear = Wildthings.get_bear(id)
-    %Conv{conv | status: 200, resp_body: "<h1> Bear #{bear.id} - #{bear.name}</h1>"}
+
+    %Conv{conv | status: 200, resp_body: BearView.show(bear) }
   end
 
   def create(%Conv{} = conv) do
@@ -28,9 +29,5 @@ defmodule Servy.BearController do
 
   def delete(%Conv{} = conv) do
     %Conv{conv | status: 403, resp_body: "Deleting a bear is forbidden!"}
-  end
-
-  defp bear_item(bear) do
-    "<li>#{bear.name} - #{bear.type}</li>"
   end
 end
