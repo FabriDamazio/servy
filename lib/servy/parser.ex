1fd2abf34350646bc5a36c60547414acbeac6a44
Fabricio Damazio <fabridamazio@gmail.com>
2024-02-26 10:42:00 -0300

    Class 14 - Recursion

--- lib/servy/parser.ex
+++ lib/servy/parser.ex
@@ -3,14 +3,27 @@ defmodule Servy.Parser do
 
   def parse(request) do
     [top, body] = String.split(request, "\n\n")
-    [request_info | _headers] = String.split(top, "\n")
+    [request_info | headers] = String.split(top, "\n")
     [method, path, _protocol] = String.split(request_info, " ")
-    params = decode_query_params(body)
 
-    %Conv{method: method, path: path, params: params}
+    headers = parse_headers(headers, %{})
+    params = decode_query_params(headers["content-type"], body)
+
+    %Conv{method: method, path: path, params: params, headers: headers}
   end
 
-  defp decode_query_params(params) do
+  defp parse_headers([head | tail], headers) do
+    [key, value] = String.split(head, ": ")
+    headers = Map.put(headers, key, value)
+
+    parse_headers(tail, headers)
+  end
+
+  defp parse_headers([], headers), do: headers
+
+  defp decode_query_params("application/x-www-form-urlencoded", params) do
     params |> String.trim() |> URI.decode_query()
   end
+
+  defp decode_query_params(_, _), do: %{}
 end
