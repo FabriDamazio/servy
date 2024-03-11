5a3d38377ee158a3902a6f9a219dbcca963c7c4a
Fabricio Damazio <fabridamazio@gmail.com>
2024-03-11 11:14:55 -0300

    Class 27 - Another GenServer

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -2,10 +2,9 @@ defmodule Servy.Handler do
   @moduledoc """
   Handles HTTP requests.
   """
-  alias Servy.Tracker
-  alias Servy.VideoCam
   alias Servy.Conv
   alias Servy.BearController
+  alias Servy.SensorServer
   import Servy.Plugins, only: [rewrite_path: 1, track: 1, log: 1]
   import Servy.Parser, only: [parse: 1]
   import Servy.FileHandler, only: [handle_file: 2]
@@ -32,16 +31,9 @@ defmodule Servy.Handler do
   end
 
   def route(%Conv{method: "GET", path: "/sensors"} = conv) do
-    task = Task.async(fn -> Tracker.get_location("bigfoot") end)
+    sensor_data = SensorServer.get_sensor_data()
 
-    snapshots =
-      ["cam-1", "cam-2", "cam-3"]
-      |> Enum.map(&Task.async(fn -> VideoCam.get_snapshot(&1) end))
-      |> Enum.map(&Task.await/1)
-
-    where_is_bigfoot = Task.await(task)
-
-    %Conv{conv | status: 200, resp_body: inspect({snapshots, where_is_bigfoot})}
+    %Conv{conv | status: 200, resp_body: inspect(sensor_data)}
   end
 
   def route(%Conv{method: "GET", path: "/hibernate/" <> time} = conv) do
