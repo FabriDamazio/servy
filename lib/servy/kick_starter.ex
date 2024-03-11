882bb556de2d13847177401784dfecdbdb9f1ee1
Fabricio Damazio <fabridamazio@gmail.com>
2024-03-11 11:33:21 -0300

    Class 29 - Linking Processes

---
+++ lib/servy/kick_starter.ex
@@ -0,0 +1,36 @@
+defmodule Servy.KickStarter do
+  use GenServer
+  require Logger
+
+  def start do
+    Logger.info("Starting the Kickstarter...")
+    GenServer.start(__MODULE__, :ok, name: __MODULE__)
+  end
+
+  def get_server do
+    GenServer.call __MODULE__, :get_server
+  end
+
+  def init(:ok) do
+    Process.flag(:trap_exit, true)
+    Logger.info("Starting HTTP Sever...")
+    server_pid = start_http_server()
+    {:ok, server_pid}
+  end
+
+  def handle_info({:EXIT, _pid, reason}, _state) do
+    Logger.error("HTTP Server exited with reason: #{inspect reason}")
+    server_pid = start_http_server()
+    {:noreply, server_pid}
+  end
+
+  def handle_call(:get_server, _from, state) do
+    {:reply, state, state}
+  end
+
+  defp start_http_server() do
+    server_pid = spawn_link(Servy.HttpServer, :start, [4000])
+    Process.register(server_pid, :http_server)
+    server_pid
+  end
+end
