7e354bb06b1b6fec0358170e94d23a7b6e9aee21
FabriDamazio <fabridamazio@gmail.com>
2025-05-19 10:32:38 -0300

    HTML files serving

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -1,6 +1,8 @@
 defmodule Servy.Handler do
   require Logger
 
+  @pages_path Path.expand("../../pages", __DIR__)
+
   def handle(request) do
     request
     |> parse()
@@ -43,6 +45,27 @@ defmodule Servy.Handler do
     %{method: method, path: path, resp_body: "", status: nil}
   end
 
+  def route(%{method: "GET", path: "/about"} = conv) do
+      @pages_path
+      |> Path.join("about.html")
+      |> File.read()
+      |> handle_file(conv)
+  end
+
+  def route(%{method: "GET", path: "/bears/new"} = conv) do
+      @pages_path
+      |> Path.join("form.html")
+      |> File.read()
+      |> handle_file(conv)
+  end
+
+  def route(%{method: "GET", path: "/pages/" <> file} = conv) do
+      @pages_path
+      |> Path.join(file <> ".html")
+      |> File.read()
+      |> handle_file(conv)
+  end
+
   def route(%{method: "GET", path: "/wildthings"} = conv) do
     %{conv | status: 200, resp_body: "Bears, Lions, Tigers"}
   end
@@ -61,6 +84,14 @@ defmodule Servy.Handler do
 
   def route(conv) do
     %{conv | status: 404, resp_body: "No #{conv.path} here!"}
+  end
+
+  def handle_file(result, conv) do
+    case result do
+      {:ok, content} -> %{conv | status: 200, resp_body: content}
+      {:error, :enoent} -> %{conv | status: 404, resp_body: "File not found!"}
+      {:error, reason} -> %{conv | status: 500, resp_body: "File error: #{reason}"}
+    end
   end
 
   def format_response(conv) do
@@ -86,7 +117,7 @@ defmodule Servy.Handler do
 end
 
 request = """
-GET /wildthings HTTP/1.1
+GET /about HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
