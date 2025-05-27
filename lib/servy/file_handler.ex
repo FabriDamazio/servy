b9c3d31f42d2bf0d9c1e8961f62da6ac98cb642f
FabriDamazio <fabridamazio@gmail.com>
2025-05-27 10:03:34 -0300

    Create the Conv struct

--- lib/servy/file_handler.ex
+++ lib/servy/file_handler.ex
@@ -1,10 +1,11 @@
 defmodule Servy.FileHandler do
-  def handle_file(result, conv) do
+  alias Servy.Conv
+
+  def handle_file(result, %Conv{} = conv) do
     case result do
       {:ok, content} -> %{conv | status: 200, resp_body: content}
       {:error, :enoent} -> %{conv | status: 404, resp_body: "File not found!"}
       {:error, reason} -> %{conv | status: 500, resp_body: "File error: #{reason}"}
     end
   end
-
 end
